b6fc53356d6a5fc22a520b2b4e90393411f50adb
It Is Unique Official <141048868+itisuniqueofficial@users.noreply.github.com>
2025-01-04 02:18:20 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,12 @@
-const CACHE_NAME = 'unique-official-cache-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/index.css',
-  '/script.js',
-  '/icons/icon-192x192.png',
-  '/icons/icon-512x512.png'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
-  );
-});
-
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+      return response || fetch(event.request).then(fetchResponse => {
+        return caches.open('dynamic-cache').then(cache => {
+          cache.put(event.request.url, fetchResponse.clone());
+          return fetchResponse;
+        });
+      });
     })
   );
 });
